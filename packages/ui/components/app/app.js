473fbfc40afc214d0a106445cac669bcae9f585f
Zak Brown <zakbrown93@icloud.com>
2018-04-18 06:54:56 +0100

    Reorganises the UI Package

--- packages/ui/components/app/app.js
+++ packages/ui/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from 'ui/images/logo.svg';
 import TestComponent from "component/test-component/test-component";
 import style from './app.scss';
 
