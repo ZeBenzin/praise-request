9f29bca6124d8d066c09e1ee70672fd8c5aba414
Zak Brown <zakbrown93@icloud.com>
2018-04-21 21:19:19 +0100

    WIP: Icons

---
+++ packages/component/person-icon/person-icon.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import styles from "./person-icon.scss";
+
+class PersonIcon extends PureComponent {
+  render() {
+    return <span className={styles.personIcon} />;
+  }
+}
+
+export default PersonIcon;
--- packages/ui/components/app/app.js
+++ packages/ui/components/app/app.js
@@ -2,22 +2,57 @@ import React, { Component } from "react";
 import Search from "ui/views/search";
 
 import SearchIcon from "component/search-icon/search-icon";
+import PersonIcon from "component/person-icon/person-icon";
+import WalletIcon from "component/wallet-icon/wallet-icon";
 
 import styles from "./app.scss";
 
 class App extends Component {
+  getNavLinks() {
+    return [
+      {
+        id: "search",
+        label: "Search",
+        icon: <SearchIcon small />,
+        visible: true
+      },
+      {
+        id: "account",
+        label: "Account",
+        icon: <PersonIcon />,
+        visible: true
+      },
+      {
+        id: "wallet",
+        label: "Wallet",
+        icon: <WalletIcon small />,
+        visible: true
+      }
+    ];
+  }
+
+  renderNavLink({ id, label, icon }) {
+    return (
+      <div key={id} className={styles.navLink}>
+        {icon}
+        <span className={styles.navLinkLabel}>{label}</span>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className={styles.app}>
         <div className={styles.header} />
         <div className={styles.content}>
           <div className={styles.navBar}>
-            <div className={styles.navLink}>
+            {this.getNavLinks().map(link => this.renderNavLink(link))}
+            {/*<div className={styles.navLink}>
               <SearchIcon small />
               <span className={styles.navLinkLabel}>Search</span>
             </div>
             <div className={styles.navLink}>Account</div>
-            <div className={styles.navLink}>Wallet</div>
+    <div className={styles.navLink}>Wallet</div>*/}
           </div>
           <div className={styles.mainContent}>
             <Search />
