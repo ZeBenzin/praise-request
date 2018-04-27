cea72c9794d7697e445f41f65ae65a45199e1077
Zak Brown <zakbrown93@icloud.com>
2018-04-27 21:16:03 +0100

    Stylistic improvments

--- packages/component/wallet-icon/wallet-icon.js
+++ packages/component/wallet-icon/wallet-icon.js
@@ -3,10 +3,7 @@ import styles from "./wallet-icon.scss";
 
 class SearchIcon extends PureComponent {
   render() {
-    const className = this.props.small
-      ? styles.searchIconSmall
-      : styles.searchIcon;
-    return <div className={className} />;
+    return <span className={styles.walletIcon} />;
   }
 }
 
