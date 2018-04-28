93257cb74859c2460eb5784e57388dece54e462e
Zak Brown <zakbrown93@icloud.com>
2018-04-28 14:17:00 +0100

    User/Account CRUD + OST Integration (#2)
    - Users now have CRUD on the server
    - Github accounts also have CRUD along with integration into the OST user creation API
    - Implements transactions between accounts using OST's API

---
+++ packages/server/api/resources/transaction/controller.js
@@ -0,0 +1,56 @@
+const moment = require("moment");
+const GithubAccount = require("../github-account/model");
+const githubAccountController = require("../github-account/controller");
+const utils = require("../../../utils/auth");
+const axios = require("axios");
+
+const githubBasePath = "https://api.github.com";
+
+const createTransaction = (req, res) => {
+  const toAccount = GithubAccount.findOne({ userId: req.body.to.userId }).then(
+    account => {
+      if (!account) {
+        const url = `${githubBasePath}/user/${req.body.to.userId}`;
+        return axios.get(url).then(data => {
+          if (data.data) {
+            return githubAccountController.createAccount(res, {
+              body: req.body.to
+            });
+          }
+        });
+      }
+      return account;
+    }
+  );
+
+  const fromAccount = GithubAccount.findOne({ userId: req.body.from.userId });
+
+  Promise.all([toAccount, fromAccount])
+    .then(accounts => {
+      const toUUID = accounts[0]._doc.ostId;
+      const fromUUID = accounts[1]._doc.ostId;
+      const timestamp = moment().unix();
+      const queryString = utils.generateQueryString(
+        timestamp,
+        "/transaction-types/execute",
+        { from_uuid: fromUUID, to_uuid: toUUID, transaction_kind: "Praise" }
+      );
+      const signature = utils.generateSignature(queryString);
+      const url = `https://playgroundapi.ost.com${queryString}&signature=${signature}`;
+      const body = {
+        api_key: process.env.API_KEY,
+        to_uuid: toUUID,
+        from_uuid: fromUUID,
+        transaction_kind: "Praise",
+        request_timestamp: timestamp,
+        signature
+      };
+      return axios.post(url, body);
+    })
+    .then(({ data }) => {
+      return res.status(200).json(data);
+    })
+    .catch(err => res.json({ code: 400, message: err.message }));
+};
+
+module.exports = { createTransaction };
--- packages/ui/views/repo.js
+++ packages/ui/views/repo.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 import { getRepoById } from "ui/api/repo";
 
