e5f59b7536facc594cf7a1cbff05b2a284ef2ede
Zak Brown <zakbrown93@icloud.com>
2018-05-07 13:20:59 +0100

    Remove spaces

--- rpm/build.sh
+++ rpm/build.sh
@@ -1,4 +1,4 @@
-export BASE_BUILD_DIR = "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export DIR_NAME = $(basename $BASE_BUILD_DIR)
+export BASE_BUILD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export DIR_NAME=$(basename $BASE_BUILD_DIR)
 
 rpmbuild --bb praise-request.spec
