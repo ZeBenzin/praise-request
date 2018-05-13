ccc1ea31224794fab5ae7ba4c20bbe88d55b187e
Zak Brown <zakbrown93@icloud.com>
2018-05-13 18:43:37 +0100

    Ooph

--- packages/ui/scripts/build.sh
+++ packages/ui/scripts/build.sh
@@ -4,8 +4,8 @@ PROJECT_ROOT="$(cd ../../ && pwd)"
 
 node scripts/build.js
 rm -rf $PROJECT_ROOT/build/packages/ui
-mkdir -p $PROJECT_ROOT/build/packages/ui/build
-mv build $PROJECT_ROOT/build/packages/ui/build
+mkdir -p $PROJECT_ROOT/build/packages/ui
+mv build $PROJECT_ROOT/build/packages/ui
 cp -r {pm2.json,package.json,server} $PROJECT_ROOT/build/packages/ui/build
 cd $PROJECT_ROOT
 rm -rf node_modules
