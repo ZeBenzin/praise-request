1af0a2f263d07a474f26e2009e657f22ec4a14dd
Zak Brown <zakbrown93@icloud.com>
2018-05-13 19:44:40 +0100

    Refactor Build Process (UI) (#10)
    - Abstract npm build script to a bash file
    - `cp`s only the necessary modules to run the node server
    - Stops auto opening of browser on successful compilation
    - Applies technique to server package

---
+++ packages/ui/scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECT_ROOT="$(cd ../../ && pwd)"
+
+node scripts/build.js
+rm -rf $PROJECT_ROOT/build/packages/ui
+mkdir -p $PROJECT_ROOT/build/packages/ui
+mv build $PROJECT_ROOT/build/packages/ui
+cp -r {pm2.json,package.json,server} $PROJECT_ROOT/build/packages/ui/build
+cd $PROJECT_ROOT
+rm -rf node_modules
+yarn install --production
+cp -r node_modules build/packages/ui/build
