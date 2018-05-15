806dd741d69f393f5620f44f0a2604fb7010a7cc
Zak Brown <zakbrown93@icloud.com>
2018-05-15 20:40:40 +0100

    Very wip

--- packages/server/api/resources/transaction/controller.js
+++ packages/server/api/resources/transaction/controller.js
@@ -9,30 +9,29 @@ const githubBasePath = "https://api.github.com";
 const ostBasePath = "https://playgroundapi.ost.com";
 
 const getCurrentTransactions = () => {
-  // return setInterval(() => {
-  //   const timestamp = moment().unix();
-  //   const queryString = utils.generateQueryString(
-  //     timestamp,
-  //     "/transaction-types/status",
-  //     {
-  //       "transaction_uuids[]":
-  //         "0xc086ade501433872982753e70036633d0884bd6b84f67be1a438c2d34db26d4d"
-  //     }
-  //   );
-  //   const signature = utils.generateSignature(queryString);
-  //   const body = {
-  //     api_key: process.env.API_KEY,
-  //     request_timestamp: timestamp,
-  //     "transaction_uuids[]":
-  //       "0xc086ade501433872982753e70036633d0884bd6b84f67be1a438c2d34db26d4d",
-  //     signature
-  //   };
-  //   const url = `${ostBasePath}${queryString}&signature=${signature}`;
-  //   axios.post(url, body).then(data => {
-  //     console.log(data.data);
-  //   });
-  // }, 1000);
-  return 10;
+  return setInterval(() => {
+    const timestamp = moment().unix();
+    const queryString = utils.generateQueryString(
+      timestamp,
+      "/transaction-types/status",
+      {
+        transaction_uuids: "60b91a69-21e9-4d54-9bd4-fa970198801a"
+      }
+    );
+    const signature = utils.generateSignature(queryString);
+    const body = {
+      api_key: process.env.API_KEY,
+      request_timestamp: timestamp,
+      transaction_uuids: "60b91a69-21e9-4d54-9bd4-fa970198801a",
+      signature
+    };
+    const url = `${ostBasePath}${queryString}&signature=${signature}`;
+    axios.defaults.headers.post["Content-Type"] =
+      "application/x-www-form-urlencoded";
+    axios.post(url, body).then(data => {
+      console.log(data.data);
+    });
+  }, 1000);
 };
 
 const createTransaction = (req, res, next) => {
