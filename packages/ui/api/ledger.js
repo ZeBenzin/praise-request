c70f25a03505a9b87629088cb4d82954faffda66
Zak Brown <zakbrown93@icloud.com>
2018-08-05 12:45:42 +0100

    Filter transactions on timeline brush
    - Brushing the timeline filters the list below
    - Adds error handling to API balancea and ledger requests

--- packages/ui/api/ledger.js
+++ packages/ui/api/ledger.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export const getTransactions = ({ pageNo = 1 }) => {
-  console.log("pageNo", pageNo);
   return axios
     .get(`/praise/ledger?page_no=${pageNo}`)
-    .then(results => results.data.data);
+    .then(results => results.data.data)
+    .catch(err => {
+      console.error(err);
+      return { transactions: [] };
+    });
 };
