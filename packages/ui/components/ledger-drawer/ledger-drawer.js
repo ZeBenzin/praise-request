1963805eac13e8100dba34f1f9f1d9449653cd0b
Zak Brown <zakbrown93@icloud.com>
2018-08-05 12:45:42 +0100

    Filter transactions on timeline brush
    - Brushing the timeline filters the list below
    - Adds error handling to API balancea and ledger requests

--- packages/ui/components/ledger-drawer/ledger-drawer.js
+++ packages/ui/components/ledger-drawer/ledger-drawer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import debounce from "lodash/debounce";
+
 import TransactionPanel from "ui/components/transaction-panel/transaction-panel";
 import BalancePanel from "ui/components/balance-panel/balance-panel";
 import Drawer from "component/drawer/drawer";
@@ -17,10 +19,15 @@ class LedgerDrawer extends Component {
     this.state = {
       userBalance: 0,
       transactions: [],
-      pageNo: 1
+      pageNo: 1,
+      isFilterActive: false
     };
 
     this.getTransactions = this.getTransactions.bind(this);
+    this.onTransactionsFiltered = debounce(
+      this.onTransactionsFiltered.bind(this),
+      300
+    );
   }
 
   componentDidMount() {
@@ -33,11 +40,17 @@ class LedgerDrawer extends Component {
     });
   }
 
+  onTransactionsFiltered({ startIndex, endIndex }) {
+    this.setState(prevState => ({
+      transactions: [...prevState.transactions.slice(startIndex, endIndex + 1)],
+      isFilterActive: true
+    }));
+  }
+
   getTransactions() {
-    if (!this._isFetching) {
+    if (!this._isFetching && !this.state.isFilterActive) {
       this._isFetching = true;
       return getTransactions({ pageNo: this.state.pageNo + 1 }).then(result => {
-        // if results has come back empty don't update the pageNo
         this.setState(prevState => ({
           transactions: [...prevState.transactions, ...result.transactions],
           pageNo: prevState.pageNo + 1
@@ -54,6 +67,7 @@ class LedgerDrawer extends Component {
           userBalance={parseInt(this.state.userBalance, 10)}
           userData={this.props.userData}
           transactions={this.state.transactions}
+          onTransactionsFiltered={this.onTransactionsFiltered}
         />
         <TransactionPanel
           userBalance={this.state.userBalance}
